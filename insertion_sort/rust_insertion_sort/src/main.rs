b93ea285298eee9a96d5606626fb4f4df61a3196
Richard W. Norton <rwtnorton@gmail.com>
2024-02-12 14:55:09 -0800

    remove silly hello world

--- insertion_sort/rust_insertion_sort/src/main.rs
+++ insertion_sort/rust_insertion_sort/src/main.rs
@@ -1,7 +1,6 @@
 mod insertion_sort;
 
 fn main() {
-    println!("Hello, world!");
     let mut nums = vec![2, 1, 0, 5, 4, 3];
     println!("{:?}", nums);
     insertion_sort::insertion_sort(&mut nums);
